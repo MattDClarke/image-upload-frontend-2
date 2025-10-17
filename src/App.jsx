506fc702e37f8da819c00c7e34bfcf07db1abb65
Matthew Clarke <m.d.clarke.sa@gmail.com>
2025-10-17 16:12:12 +0200

    add: image upload component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ImgUploader from './components/ImgUploader';
 
 function App() {
-
   return (
     <div className="App">
-      <p>TODO: add image upload</p>    
+      <ImgUploader />
+      <ToastContainer />
     </div>
   );
 }
